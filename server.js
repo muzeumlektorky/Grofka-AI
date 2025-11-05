16fe1c38c885cb68ada167506785877cc03e6069
muzeumlektorky <muzeumlektorky@gmail.com>
2025-11-05 11:15:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const app = express();
 // Načítanie vedomostí pre chatbota
 const knowledgePath = path.resolve("informacie/vedomosti.txt");
 const assistantContext = fs.readFileSync(knowledgePath, "utf-8");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 
 
 // Povolenie CORS pre všetky zdroje
@@ -21,6 +25,12 @@ app.use(express.json());
 
 // Servovanie statických súborov (HTML, CSS, JS)
 app.use(express.static("public"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Inicializácia OpenAI klienta
 const client = new OpenAI({
@@ -65,4 +75,5 @@ fs.watchFile(knowledgePath, () => {
   assistantContext = fs.readFileSync(knowledgePath, "utf-8");
   console.log("🟢 Aktualizované vedomosti pre Grófku načítané.");
 });
+
 
